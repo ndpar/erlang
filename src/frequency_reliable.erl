4eb0a32f97f4aa8fd4dfb3050f0a7d1f1158dd10
Andrey Paramonov <git@ndpar.com>
2009-11-16 20:40:07 -0500

    Added processor link to implement reliability

--- src/frequency_reliable.erl
+++ src/frequency_reliable.erl
@@ -4,6 +4,19 @@
 %%
 %% Based on frequency2.erl
 %%
+%% 1> frequency_reliable:start().
+%% ok
+%% 2> frequency_reliable:allocate().
+%% {ok,10}
+%% 3> frequency_reliable:allocate().
+%% {ok,11}
+%% 4> exit(self(), kill).
+%% ** exception exit: killed
+%% 5> frequency_reliable:allocate().
+%% {ok,10}
+%% 6> frequency_reliable:allocate().
+%% {ok,11}
+%%
 -module(frequency_reliable).
 -export([start/0, stop/0]).
 -export([allocate/0, deallocate/1]).
@@ -15,6 +28,7 @@ start() ->
     register(frequency_reliable, spawn(frequency_reliable, init, [])), ok.
 
 init() ->
+    process_flag(trap_exit, true),
     Frequencies = {get_frequencies(), []},
     loop(Frequencies).
 
@@ -38,7 +52,19 @@ loop(Frequencies) ->
                 _ ->
                     reply(Pid, {error, frequencies_in_use}),
                     loop(Frequencies)
-            end
+            end;
+        {'EXIT', Pid, _Reason} ->
+            NewFrequencies = exited(Frequencies, Pid),
+            loop(NewFrequencies)
+    end.
+
+exited({Free, Allocated}, Pid) ->
+    case lists:keysearch(Pid, 2, Allocated) of
+        {value, {Freq, Pid}} ->
+            NewAllocated = lists:keydelete(Freq, 1, Allocated),
+            exited({[Freq|Free], NewAllocated}, Pid);
+        false ->
+            {Free, Allocated}
     end.
 
 allocate({[], Allocated}, _Pid) ->
@@ -48,12 +74,15 @@ allocate({[Freq|Free], Allocated}, Pid) ->
     ClientFrequencies = my_lists:keyfilter(Pid, 2, Allocated),
     case length(ClientFrequencies) of
         3 -> {{[Freq|Free], Allocated}, {error, exceed_limit}};
-        _ -> {{Free, [{Freq,Pid}|Allocated]}, {ok, Freq}}
+        _ ->
+            link(Pid),
+            {{Free, [{Freq,Pid}|Allocated]}, {ok, Freq}}
     end.
 
 deallocate({Free, Allocated}, Freq, Pid) ->
     case lists:member({Freq, Pid}, Allocated) of
         true ->
+            unlink(Pid),
             NewAllocated = lists:keydelete(Freq, 1, Allocated),
             {{[Freq|Free], NewAllocated}, ok};
         _ ->
