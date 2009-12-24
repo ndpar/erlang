07f853267bb1335254d36490120f03818c4b47d9
Andrey Paramonov <git@ndpar.com>
2009-12-23 23:00:49 -0500

    Introduced ErlStringMap.java

---
+++ mycache/java/src/main/java/com/ndpar/erlcache/ErlStringMap.java
@@ -0,0 +1,104 @@
+package com.ndpar.erlcache;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import com.ericsson.otp.erlang.*;
+
+public class ErlStringMap implements Map<String, String> {
+
+    private final OtpSelf self;
+    private final OtpPeer other;
+    private final String cacheModule;
+
+    public ErlStringMap(String client, String cookie, String serverNode, String cacheModule) {
+        try {
+            self = new OtpSelf(client, cookie);
+            other = new OtpPeer(serverNode);
+            this.cacheModule = cacheModule;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    public String put(String key, String value) {
+        return remoteCall("put", key, value);
+    }
+
+    public String get(Object key) {
+        return remoteCall("get", (String) key);
+    }
+
+    public String remove(Object key) {
+        return remoteCall("remove", (String) key);
+    }
+
+    private String remoteCall(String method, String... args) {
+        try {
+            OtpConnection connection = self.connect(other);
+            connection.sendRPC(cacheModule, method, stringsToErlangStrings(args));
+            OtpErlangObject received = connection.receiveRPC();
+            connection.close();
+            return parse(received);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    private OtpErlangObject[] stringsToErlangStrings(String[] strings) {
+        OtpErlangObject[] result = new OtpErlangObject[strings.length];
+        for (int i = 0; i < strings.length; i++) result[i] = new OtpErlangString(strings[i]);
+        return result;
+    }
+
+    private String parse(OtpErlangObject otpObj) {
+        if (otpObj instanceof OtpErlangAtom) {
+            OtpErlangAtom atom = (OtpErlangAtom) otpObj;
+            if (atom.atomValue().equals("null")) return null;
+            else throw new IllegalArgumentException("Only atom null is supported");
+
+        } else if (otpObj instanceof OtpErlangString) {
+            OtpErlangString str = (OtpErlangString) otpObj;
+            return str.stringValue();
+        }
+        throw new IllegalArgumentException("Unexpected type " + otpObj.getClass().getName());
+    }
+
+
+    public void clear() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean containsKey(Object key) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean containsValue(Object value) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Set entrySet() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Set keySet() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void putAll(Map m) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Collection values() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ mycache/java/src/test/java/com/ndpar/erlcache/ErlStringMapTest.java
@@ -0,0 +1,75 @@
+package com.ndpar.erlcache;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ErlStringMapTest {
+
+    private static String cookie;
+    private Map<String, String> map;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        cookie = FileUtils.readFileToString(new File("/Users/andrey/.erlang.cookie"));
+    }
+
+    @Before
+    public void setUp() {
+        map = new ErlStringMap("client1", cookie, "slave1@macBook", "mycache");
+    }
+
+    @Test
+    public void getReturnsNullFromEmptyMap() {
+        Object result = map.get("testing");
+        assertNull(result);
+    }
+
+    @Test
+    public void getPutRemoveReturnCorrectValues() {
+        assertNull(map.put("foo", "bar"));
+        assertEquals("bar", map.get("foo"));
+        assertEquals("bar", map.put("foo", "baz"));
+        assertEquals("baz", map.remove("foo"));
+    }
+
+    @Test
+    public void removeReturnsNullIfNoSuchEntry() {
+        Object result = map.remove("notexsist");
+        assertNull(result);
+    }
+
+    @Test @Ignore("long running dirty test")
+    public void writePerformance() {
+        int count = 1000;
+        long start = System.currentTimeMillis();
+        for (Integer i = 0; i < count; i++) map.put(i.toString(), i.toString());
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(count + " write operations take: " + duration + "ms. " + (1.0 * duration/count) + "ms per operation");
+    }
+
+    @Test @Ignore("long running dirty test")
+    public void removePerformance() {
+        int count = 1000;
+        long start = System.currentTimeMillis();
+        for (Integer i = 0; i < count; i++) map.remove(i.toString());
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(count + " remove operations take: " + duration + "ms. " + (1.0 * duration/count) + "ms per operation");
+    }
+
+    @Test @Ignore("long running test")
+    public void getPerformance() {
+        int count = 1000;
+        long start = System.currentTimeMillis();
+        for (Integer i = 0; i < count; i++) map.get(i.toString());
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(count + " get operations take: " + duration + "ms. " + (1.0 * duration/count) + "ms per operation");
+    }
+}
