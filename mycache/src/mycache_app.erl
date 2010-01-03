ea61315b205d32f2377826a9196e052fe908eccd
Andrey Paramonov <git@ndpar.com>
2010-01-03 14:42:33 -0500

    Tried edoc annotations

--- mycache/src/mycache_app.erl
+++ mycache/src/mycache_app.erl
@@ -1,9 +1,25 @@
+%%
+%% @author Andrey Paramonov <erlang@ndpar.com>
+%% @doc Application module for Distributed Cache
+%% @copyright 2009 Andrey Paramonov
+%%
 -module(mycache_app).
 -export([start/2, stop/1]).
 -behaviour(application).
 
+%%
+%% @doc Starts application
+%% @spec start(Type, StartArgs) -> Result
+%% Result = {ok,Pid} | ignore | {error,Error}
+%% Pid = pid()
+%% Error = {already_started,Pid} | shutdown | term()
+%%
 start(_Type, _StartArgs) ->
     mycache_sup:start().
 
+%%
+%% @doc Stops application
+%% @spec stop(State) -> ok
+%%
 stop(_State) ->
     ok.
