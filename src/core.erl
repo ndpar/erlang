5ec75874da670ae78ae4e1412df6e29dae7cdb22
Andrey Paramonov <github@ndpar.com>
2017-12-30 15:27:06 -0500

    modules.erl

--- src/core.erl
+++ src/core.erl
@@ -2,10 +2,12 @@
 % Erlang implementation of some functions from clojure.core.
 %
 -module(core).
--export([frequencies/1]).
+-export([frequencies/1, group_by/2]).
 
 %
 % Returns a map from distinct items in List to the number of times they appear.
+%
+% See https://clojuredocs.org/clojure.core/frequencies
 %
 frequencies(List) ->
   lists:foldl(fun update_count/2, #{}, List).
@@ -13,9 +15,27 @@ frequencies(List) ->
 update_count(X, Map) ->
   maps:update_with(X, fun(C) -> C + 1 end, 1, Map).
 
+%
+% Returns a map of the elements of List keyed by the result of
+% Fun on each element. The value at each key will be a list of the
+% corresponding elements, in the order they appeared in List.
+%
+% See https://clojuredocs.org/clojure.core/group-by
+%
+group_by(Fun, List) ->
+  F = fun(E, Map) ->
+    K = Fun(E),
+    maps:update_with(K, fun(L) -> [E | L] end, [E], Map)
+  end,
+  maps:map(fun(_, V) -> lists:reverse(V) end, lists:foldl(F, #{}, List)).
+
 
 -include_lib("eunit/include/eunit.hrl").
 
 frequencies_test() ->
   ?assertEqual(#{1=>2, 2=>2, 3=>3, 4=>1},
     frequencies([1, 2, 3, 2, 3, 4, 1, 3])).
+
+group_by_test() ->
+  ?assertEqual(#{1=>["a"], 2=>["as", "aa"], 3=>["asd"], 4=>["asdf", "qwer"]},
+    group_by(fun erlang:length/1, ["a", "as", "asd", "aa", "asdf", "qwer"])).
---
+++ src/modules.erl
@@ -0,0 +1,56 @@
+%
+% Various functions to work with modules. Based on
+% J. Armstrong, Programming Erlang, Chapter 8 exercises
+%
+-module(modules).
+-export([all_loaded/0, is_loaded/1, exported/1]).
+
+%
+% Returns a list of all modules that have been loaded in ERTS.
+%
+all_loaded() -> [M || {M, _} <- code:all_loaded()].
+
+%
+% Returns true if the Module has been loaded.
+%
+is_loaded(Module) -> lists:member(Module, all_loaded()).
+
+%
+% Returns a list of functions exported by the Module.
+%
+exported(Module) -> proplists:get_value(exports, Module:module_info()).
+
+
+-include_lib("eunit/include/eunit.hrl").
+
+is_loaded_test() ->
+  ?assert(is_loaded(?MODULE)).
+
+exported_test() ->
+  ?assert(lists:member({test, 0}, exported(?MODULE))).
+
+% The following tests are done in Erlang 20.2.2
+
+% Exercise 1, p.138
+dict_test() ->
+  ?assertEqual(23, length(exported(dict))).
+
+% Exercise 2, p.139
+module_with_most_functions_test() ->
+  ?assertMatch({erlang, _}, % 329
+    lists:last(lists:keysort(2, [{M, length(exported(M))} || M <- all_loaded()]))).
+
+% Exercise 2, p.139
+most_common_function_name_test() ->
+  AllFunctions = [F || M <- all_loaded(), {F, _} <- exported(M)],
+  Functions = lists:keysort(2, maps:to_list(core:frequencies(AllFunctions))),
+  ?assertMatch({module_info, _}, lists:last(Functions)). % 284
+
+% Exercise 2, p.139
+all_unambiguous_functions_test() ->
+  AllFunctions = lists:usort([{F, M} || M <- all_loaded(), {F, _} <- exported(M)]),
+  GroupFunctions = core:group_by(fun({F, _}) -> F end, AllFunctions),
+  SingleModuleFuns = lists:filter(fun({_, L}) -> length(L) =:= 1 end, maps:to_list(GroupFunctions)),
+  Result = lists:flatmap(fun({_, L}) -> L end, SingleModuleFuns),
+  %io:format("~p~n", [Result]), % 1447 functions
+  ?assert(lists:member({'*', erlang}, Result)).
