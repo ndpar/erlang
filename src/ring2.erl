14ac8ecfe1a795ba9e3aa1caa799ae03918d7a72
Andrey Paramonov <github@ndpar.com>
2020-03-21 22:50:40 -0400

    ring2: wait for main process to finish

--- src/ring2.erl
+++ src/ring2.erl
@@ -4,12 +4,10 @@
 %
 % Compile with trace disabled:
 % 1> c(ring2, {d, no_trace}).
-%
-% Run from the shell:
-% $ time erl -noshell -pa ebin -eval 'ring2:start(200000, 2000000, hello)' -s init stop
+% 2> ring2:measure_time(10000, 10000, hello).
 %
 -module(ring2).
--export([start/3]).
+-export([measure_time/3, start/3]).
 
 -ifndef(no_trace).
 -define(TRACE(P, M, Message), io:format("~p -> ~p: ~p (~p)~n", [self(), P, Message, M])).
@@ -17,12 +15,20 @@
 -define(TRACE(P, M, Message), void).
 -endif.
 
+measure_time(N, M, Message) ->
+    {Micros, ok} = timer:tc(?MODULE, start, [N, M, Message]),
+    io:format("Done in ~p µs. Message passing speed ~p s~n", [Micros, Micros / N / M / 1000000.0]).
+
 %
 % Creates N processes in a ring.
 % Sends a message round the ring M times
 % so that a total of N*M messages get sent.
 %
-start(N, M, Message) when N > 1 -> spawn(fun() -> ring(N-1, M, Message) end).
+start(N, M, Message) when N > 1 ->
+    {_Pid, MonitorRef} = spawn_monitor(fun() -> ring(N-1, M, Message) end),
+    receive
+        {_Tag, MonitorRef, _Type, _Object, _Info} -> ok
+    end.
 
 ring(N, M, Message) when M > 0 ->
     P = lists:foldl(fun(_, Proc) -> spawn(fun() -> loop(Proc) end) end, self(), lists:seq(1, N)),
