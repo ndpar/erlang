f453a058e96039ed6e53ea5a34949213ffddf260
Andrey Paramonov <github@ndpar.com>
2020-12-13 00:01:44 -0500

    Rewrite Sleeping Barber using `gen_statem`

--- barber/barber.erl
+++ barber/barber.erl
@@ -1,16 +1,15 @@
 -module(barber).
 -author("Andrey Paramonov <github@ndpar.com>").
--behaviour(gen_fsm).
+-behaviour(gen_statem).
 
 %% API
--export([start_link/0, new_customer/1]).
+-export([open_shop/0, new_customer/1]).
 
 %% Callback functions
--export([init/1, handle_event/3, handle_sync_event/4, handle_info/3,
-         terminate/3, code_change/4]).
+-export([init/1, callback_mode/0, terminate/3, code_change/4]).
 
 %% FSM states
--export([busy/2, sleep/2]).
+-export([busy/3, sleep/3]).
 
 -define(SERVER, ?MODULE).
 -define(HAIRCUT_TIME, 5000).
@@ -22,78 +21,73 @@
 %% API
 %%====================================================================
 
-start_link() ->
-    gen_fsm:start_link({local, ?SERVER}, ?MODULE, [], []).
+open_shop() ->
+  gen_statem:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+-spec new_customer(pid()) -> ok.
 new_customer(Customer) ->
-    gen_fsm:send_event(?SERVER, {new, Customer}).
+  gen_statem:cast(?SERVER, {new, Customer}).
 
 %%====================================================================
 %% gen_fsm callbacks
 %%====================================================================
 
-init([]) ->
-    log("Shop is open. zzzzZ"),
-    {ok, sleep, #state{}}.
+callback_mode() ->
+  state_functions.
 
-handle_info(finish, busy, #state{chair = Customer, room = Room}) ->
-    log("Do you like your haircut ~p?", [Customer]),
-    customer:done(Customer),
-    case Room of
-        [C | Rest] ->
-            log("Next please ~p.", [C]),
-            serving(C),
-            NewStateData = #state{chair = C, room = Rest},
-            {next_state, busy, NewStateData};
-        [] ->
-            log("Time for nap. zzzzZ."),
-            {next_state, sleep, #state{}}
-    end.
+init([]) ->
+  log("Shop is open. zzzzZ"),
+  {ok, sleep, #state{}}.
 
 serving(Customer) ->
-    customer:sit_down(Customer),
-    timer:send_after(?HAIRCUT_TIME, finish).
-
-handle_event(_Event, _StateName, StateData) ->
-    {stop, undefined, StateData}.
-
-handle_sync_event(_Event, _From, _StateName, StateData) ->
-    {stop, undefined, none, StateData}.
+  customer:sit_down(Customer),
+  timer:send_after(?HAIRCUT_TIME, finish).
 
 terminate(_Reason, _StateName, _StateData) ->
-    ok.
+  ok.
 
 code_change(_OldVsn, StateName, StateData, _Extra) ->
-    {ok, StateName, StateData}.
+  {ok, StateName, StateData}.
 
 %%====================================================================
 %% FSM states and transitions
 %%====================================================================
 
-sleep({new, Customer}, StateData) ->
-    log("Good morning ~p. Please take a seat.", [Customer]),
-    serving(Customer),
-    NewStateData = StateData#state{chair = Customer},
-    {next_state, busy, NewStateData}.
+sleep(cast, {new, Customer}, StateData) ->
+  log("Good morning ~p. Please take a seat.", [Customer]),
+  serving(Customer),
+  {next_state, busy, StateData#state{chair = Customer}}.
 
-busy({new, Customer}, #state{room = Room} = StateData)
-  when length(Room) == ?ROOM_SIZE ->
-    log("Sorry ~p. Not today.", [Customer]),
-    customer:sorry(Customer),
-    {next_state, busy, StateData};
+busy(info, finish, #state{chair = Customer, room = Room}) ->
+  log("Do you like your haircut ~p?", [Customer]),
+  customer:done(Customer),
+  case Room of
+    [C | Rest] ->
+      log("Next please ~p.", [C]),
+      serving(C),
+      NewStateData = #state{chair = C, room = Rest},
+      {next_state, busy, NewStateData};
+    [] ->
+      log("Time for nap. zzzzZ."),
+      {next_state, sleep, #state{}}
+  end;
 
-busy({new, Customer}, #state{room = Room} = StateData) ->
-    log("I'm busy. You have to wait."),
-    customer:wait(Customer),
-    NewStateData = StateData#state{room = Room ++ [Customer]},
-    {next_state, busy, NewStateData}.
+busy(cast, {new, Customer}, #state{room = Room} = StateData) when length(Room) == ?ROOM_SIZE ->
+  log("Sorry ~p. Not today.", [Customer]),
+  customer:sorry(Customer),
+  {next_state, busy, StateData};
+
+busy(cast, {new, Customer}, #state{room = Room} = StateData) ->
+  log("I'm busy. You have to wait."),
+  customer:wait(Customer),
+  {next_state, busy, StateData#state{room = Room ++ [Customer]}}.
 
 %%====================================================================
 %% Helper functions
 %%====================================================================
 
 log(Message) ->
-    log(Message, []).
+  log(Message, []).
 
 log(Message, Args) ->
-    error_logger:info_msg("~p: " ++ Message ++ "~n", [self() | Args]).
+  error_logger:info_msg("~p: " ++ Message ++ "~n", [self() | Args]).
--- barber/customer.erl
+++ barber/customer.erl
@@ -1,93 +1,86 @@
 -module(customer).
 -author("Andrey Paramonov <github@ndpar.com>").
--behaviour(gen_fsm).
+-behaviour(gen_statem).
 
 %% API
 -export([start/0, sit_down/1, done/1, wait/1, sorry/1]).
 
 %% Callback functions
--export([init/1, handle_event/3, handle_sync_event/4, handle_info/3,
-         terminate/3, code_change/4]).
+-export([init/1, callback_mode/0, terminate/3, code_change/4]).
 
 %% FSM states
--export([waiting/2, served/2]).
+-export([waiting/3, served/3]).
 
 %%====================================================================
 %% API
 %%====================================================================
 
 start() ->
-    {ok,Pid} = gen_fsm:start(?MODULE, [], []),
-    Pid.
+  {ok, Pid} = gen_statem:start(?MODULE, [], []),
+  Pid.
 
 sit_down(Customer) ->
-    gen_fsm:send_event(Customer, sit_down).
+  gen_statem:cast(Customer, sit_down).
 
 done(Customer) ->
-    gen_fsm:send_event(Customer, exit).
+  gen_statem:cast(Customer, exit).
 
 wait(Customer) ->
-    gen_fsm:send_event(Customer, wait).
+  gen_statem:cast(Customer, wait).
 
 sorry(Customer) ->
-    gen_fsm:send_event(Customer, exit).
+  gen_statem:cast(Customer, exit).
 
 %%====================================================================
 %% gen_fsm callbacks
 %%====================================================================
 
+callback_mode() ->
+  state_functions.
+
 init([]) ->
-    log("Good morning."),
-    {ok, waiting, unow()}.
-
-handle_info(_Info, _StateName, StateData) ->
-    {stop, undefined, StateData}.
-
-handle_event(_Event, _StateName, StateData) ->
-    {stop, undefined, StateData}.
-
-handle_sync_event(_Event, _From, _StateName, StateData) ->
-    {stop, undefined, none, StateData}.
+  log("Good morning."),
+  {ok, waiting, unow()}.
 
 terminate(_Reason, _StateName, _StateData) ->
-    ok.
+  ok.
 
 code_change(_OldVsn, StateName, StateData, _Extra) ->
-    {ok, StateName, StateData}.
+  {ok, StateName, StateData}.
 
 %%====================================================================
 %% FSM states and transitions
 %%====================================================================
 
-waiting(sit_down, WaitStart) ->
-    log("I've been waiting for ~p sec.", [duration(WaitStart)]),
-    {next_state, served, unow()};
+waiting(cast, sit_down, WaitStart) ->
+  log("I've been waiting for ~p sec.", [duration(WaitStart)]),
+  {next_state, served, unow()};
 
-waiting(wait, StateData) ->
-    log("OK."),
-    {next_state, waiting, StateData};
+waiting(cast, wait, StateData) ->
+  log("OK."),
+  {next_state, waiting, StateData};
 
-waiting(exit, _StateData) ->
-    log("Maybe tomorrow."),
-    {stop, normal, undefined}.
+waiting(cast, exit, _StateData) ->
+  log("Maybe tomorrow."),
+  {stop, normal, undefined}.
 
-served(exit, ServiceStart) ->
-    log("Thank you. The haircut took ~p sec.", [duration(ServiceStart)]),
-    {stop, normal, undefined}.
+served(cast, exit, ServiceStart) ->
+  log("Thank you. The haircut took ~p sec.", [duration(ServiceStart)]),
+  {stop, normal, undefined}.
 
 %%====================================================================
 %% Helper functions
 %%====================================================================
 
 log(Message) ->
-    log(Message, []).
+  log(Message, []).
 
 log(Message, Args) ->
-    error_logger:info_msg("~p: " ++ Message ++ "~n", [self() | Args]).
+  error_logger:info_msg("~p: " ++ Message ++ "~n", [self() | Args]).
 
 unow() ->
-    calendar:universal_time().
+  calendar:universal_time().
 
 duration(Start) ->
-    {_, {_, _, Seconds}} = calendar:time_difference(Start, unow()),
-    Seconds.
+  {_, {_, _, Seconds}} = calendar:time_difference(Start, unow()),
+  Seconds.
