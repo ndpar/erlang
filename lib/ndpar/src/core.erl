fcd3ca6571a0df3a7e1debbb674053b70243cf8c
Andrey Paramonov <github@ndpar.com>
2020-12-27 11:29:21 -0500

    Implement Pearl 2
    from the book Pearls of Functional Algorithm Design.

--- lib/ndpar/src/core.erl
+++ lib/ndpar/src/core.erl
@@ -3,8 +3,10 @@
 %% Inspired by Clojure and Haskell.
 %%
 -module(core).
--export([frequencies/1, group_by/2, inc/1, minfree/1]).
+-export([frequencies/1, group_by/2, inc/1, minfree/1, msc/1, msc2/1]).
 -export([zipfold/4, zipfold/5]).
+
+-import(lists, [filter/2, map/2, max/1]).
 
 %%
 %% @doc Returns a map from distinct items in List to the number of times they appear.
@@ -97,6 +99,58 @@ minfrom(A, {N, List}) ->
     _ -> minfrom(A, {M, Us})
   end.
 
+
+%%
+%% @doc Maximum surpasser count.
+%%
+%% `x[j]' is a surpasser of `x[i]' if `i < j' and `x[i] < x[j]'.
+%% The <em>surpasser count</em> of an element is the number of
+%% its surpassers.
+%%
+%% The complexity of the divide and conquer version of the MSC algorithm is `O(n log n)'.
+%%
+%% See [B1] Chapter 2, pp. 7–11.
+%%
+%% @see msc2/1
+%%
+-spec msc(list()) -> non_neg_integer().
+
+msc(List) -> max([C || {_, C} <- table(List)]).
+
+
+-spec table([A]) -> [{A, Count :: non_neg_integer()}].
+
+table([X]) -> [{X, 0}];
+table(List) ->
+  M = length(List),
+  N = M div 2,
+  {Ys, Zs} = lists:split(N, List),
+  join(M - N, table(Ys), table(Zs)).
+
+-spec join(non_neg_integer(), [A], [A]) -> [A].
+
+join(0, Txs, []) -> Txs;
+join(_N, [], Tys) -> Tys;
+join(N, [{X, C} | Txst] = Txs, [{Y, D} | Tyst] = Tys) ->
+  case X < Y of
+    true -> [{X, C + N} | join(N, Txst, Tys)];
+    false -> [{Y, D} | join(N - 1, Txs, Tyst)]
+  end.
+
+
+%%
+%% @doc MSC algorithm running in quadratic time.
+%%
+-spec msc2(list()) -> non_neg_integer().
+
+msc2(List) -> max([scount(X, Xs) || [X | Xs] <- tails(List)]).
+
+scount(X, Xs) -> length(filter(fun(Y) -> X < Y end, Xs)).
+
+tails([]) -> [];
+tails([_ | Xs] = List) -> [List | tails(Xs)].
+
+
 %% =============================================================================
 %% Unit tests
 %% =============================================================================
@@ -111,11 +165,11 @@ zipfold_test_() -> [
     zipfold(fun(Acc, A, B, C) -> Acc + A * B * C end, 0, [1, 2, 3], [2, 3, 4], [3, 4, 5]))].
 
 frequencies_test() ->
-  ?assertEqual(#{1=>2, 2=>2, 3=>3, 4=>1},
+  ?assertEqual(#{1 => 2, 2 => 2, 3 => 3, 4 => 1},
     frequencies([1, 2, 3, 2, 3, 4, 1, 3])).
 
 group_by_test() ->
-  ?assertEqual(#{1=>["a"], 2=>["as", "aa"], 3=>["asd"], 4=>["asdf", "qwer"]},
+  ?assertEqual(#{1 => ["a"], 2 => ["as", "aa"], 3 => ["asd"], 4 => ["asdf", "qwer"]},
     group_by(fun erlang:length/1, ["a", "as", "asd", "aa", "asdf", "qwer"])).
 
 minfree_test_() ->
@@ -128,5 +182,21 @@ minfree_test_() ->
     ?_assertEqual(6, minfree(List)),
     ?_assertEqual(hd(lists:seq(0, 8) -- List), minfree(List))
   ].
+
+scount([X | Xs]) -> scount(X, Xs).
+
+msc_test_() ->
+  Word = "GENERATING",
+  [
+    ?_assertEqual(5, scount(hd(Word), tl(Word))),
+    ?_assertEqual([5, 6, 2, 5, 1, 4, 0, 1, 0, 0], map(fun scount/1, tails(Word))),
+    ?_assertEqual(6, msc2(Word)),
+    ?_assertEqual(6, msc(Word)),
+    ?_assertEqual([6, 6, 5, 5, 4, 4, 1, 1, 0, 0], map(fun msc/1, tails(Word)))
+  ].
+
+tails_test_() -> [
+  ?_assertEqual([[1, 2, 3], [2, 3], [3]], tails([1, 2, 3]))
+].
 
 -endif.
